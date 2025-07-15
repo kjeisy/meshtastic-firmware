86a22d55a0004b76ee1a3003be6a91971123a479
Kevin Jeisy <jeisy@me.com>
2025-07-15 07:07:43 +0200

    configure expansion board button as ALT_BUTTON

--- variants/seeed_xiao_s3/variant.h
+++ variants/seeed_xiao_s3/variant.h
@@ -35,6 +35,7 @@ L76K GPS Module Information : https://www.seeedstudio.com/L76K-GNSS-Module-for-S
 
 #define BUTTON_PIN 21 // This is the Program Button
 #define BUTTON_NEED_PULLUP
+#define ALT_BUTTON_PIN 2 // expansion board button
 
 #define BATTERY_PIN -1
 #define ADC_CHANNEL ADC1_GPIO1_CHANNEL
